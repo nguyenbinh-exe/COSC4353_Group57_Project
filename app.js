07bf032e2924f0125affa6db5e1a3cbdc4d8056e
Terry Hoang <terrytamhoang@gmail.com>
2023-03-01 22:15:44 -0600

    added mongoose

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require('ejs');
 var _ = require("lodash");
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.set('strictQuery',false);
+
+
 
 app.set("view engine",'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
+
+mongoose.connect("mongodb+srv://admin:<password>@cluster0.peg8eaz.mongodb.net/userDB");
 
 app.get('/',function (req,res){
     res.render('homepage');
