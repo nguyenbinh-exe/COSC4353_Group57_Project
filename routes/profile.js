8f4a1ff68948fdb6bc7d4345025810c0f5c48085
Tung Dinh <72676607+tsdinh442@users.noreply.github.com>
2023-03-24 22:23:39 -0500

    update to post method

--- routes/profile.js
+++ routes/profile.js
@@ -16,6 +16,16 @@ router.get('/view_profile', (req, res, next) => {
     res.render('profile_management/view_profile', {name, address, city, state, zipcode})
 });
 
+router.post('/view_profile', (req, res, next) => {
+    const body = req.body;
+    const name = body.fullname;
+    const address = body.address1;
+    const city = body.city;
+    const state = body.state;
+    const zipcode = body.zipcode;
+    res.render('profile_management/view_profile', {name, address, city, state, zipcode})
+});
+
 //router.get('/update', (req, res, next) => {
 //    res.render('profile_management/create_profile')
 //});
