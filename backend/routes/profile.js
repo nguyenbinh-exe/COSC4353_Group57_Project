f42b83aa3eb10b0e403f9a1879751813a65cc928
Tung Dinh <72676607+tsdinh442@users.noreply.github.com>
2023-04-02 14:18:41 -0500

    pull data from backend to front end

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -39,18 +39,32 @@ router.post('/added_profile', (req, res, next) => {
     .catch(err => res.status(400).json('Error: ' + err));
 
     
- });
+});
 
 router.get('/view_profile', (req, res, next) => {
-    const body = req.body;
-    const name = body.fullname;
-    const address1 = body.address1;
-    const address2 = body.address2;
-    const city = body.city;
-    const state = body.state;
-    const zipcode = body.zipcode;
-    res.render('../views/profile_management/view_profile', {name, address1, address2, city, state, zipcode})
+    const userId = '64292ade5cf824df75ccad21'; 
+
+    Client.findById(userId).exec()
+    .then((user) => {
+        if (!user) {
+        console.log('User not found');
+        } else {
+            const name = user.name;
+            const address1 = user.address1;
+            const address2 = user.address2;
+            const city = user.city;
+            const state = user.state;
+            const zipcode = user.zipcode;   
+            let display_zipcode = zipcode.toString();
+            res.render('../views/profile_management/view_profile', {name, address1, address2, city, state, display_zipcode})
+        }
+      });
+    
+    
+    
 });
+
+
 
 //router.get('/update', (req, res, next) => {
 //    res.render('profile_management/create_profile')
