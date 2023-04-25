295573e35733d2bdbd1f614fc53919c269d8042d
Terry Hoang <terrytamhoang@gmail.com>
2023-04-25 17:27:27 -0500

    Added password encryption
    Added password encryption
    be sure to install bcryptjs

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
 
 var router = express.Router();
 const ClientData = require('../models/clientInformation.model');
@@ -13,9 +15,9 @@ router.get('/register',function (req,res){
 
 router.post('/register', function(req,res) {
 
-    const newUser = new userCredentials({        
+    const newUser = new userCredentials({
         username: req.body.username,
-        password: req.body.password
+        password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
     });
 
     newUser.save()
@@ -31,6 +33,28 @@ router.post('/register', function(req,res) {
 
 /////////////// USER LOGIN //////////////////
 
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        userCredentials.findOne({ username: username })
+            .then(function(user) {
+                if (!user) {
+                    return done(null, false, { message: 'Incorrect username.' });
+                }
+                bcrypt.compare(password, user.password, function(err, isMatch) {
+                    if (err) throw err;
+                    if (!isMatch) {
+                        return done(null, false, { message: 'Incorrect password.' });
+                    }
+                    return done(null, user);
+                });
+            })
+            .catch(function(err) {
+                return done(err);
+            });
+    }
+));
+
+
 router.get('/login',function (req,res){
     res.render('login');
 })
@@ -43,9 +67,9 @@ router.get('/loggedin',function(req,res){
     }
     else{
         ClientData.findOne({userID: req.user._id})
-        .then((client) => {
-        res.render('homepageAuthenticated',{name: client.name, userid: client._id})
-        })
+            .then((client) => {
+                res.render('homepageAuthenticated',{name: client.name, userid: client._id})
+            })
     }
 
 })
